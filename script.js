9d3f817cfaadb3e7428a6685903667a871184766
such-in <sachinmaurya1710@gmail.com>
2023-04-12 23:49:59 +0530

    learning Java script

---
+++ script.js
@@ -0,0 +1,13 @@
+var count = 10,
+name;
+count-=10;
+name='Mary';
+if (count){
+    console.log("first block");
+}
+else if(name){
+    console.log("second block");
+}
+else{
+    console.log("third block")
+}
