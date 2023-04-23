43d76431719b160d6a3886c4eee7141aeab3fc62
such-in <sachinmaurya1710@gmail.com>
2023-04-23 09:02:51 +0530

    numbers

---
+++ numbers.js
@@ -0,0 +1,78 @@
+//Number is a data type in JavaScript which represents numeric data.
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+
+//Increment a Number with JavaScript
+//You can easily increment or add one to a variable with the ++ operator
+
+/*let myVar = 87;
+myVar = ++myVar;
+*/
+
+//Decrement a Number with JavaScript
+//You can easily decrement or decrease a variable by one with the -- operator.
+let myVar = 11;
+myVar = --myVar;
+
+//Create Decimal Numbers with JavaScript
+let myDecimal = 2.45;
+
+//Multiply Two Decimals with JavaScript
+const productDecimal = 2.0 * 2.5;
+
+//Divide One Decimal by Another
+const quotientDec = 4.4 / 2.0;
+
+//Finding a Remainder in JavaScript
+/*The remainder operator % gives the remainder of the division of two numbers.
+Note: The remainder operator is sometimes incorrectly referred to as the modulus operator.
+It is very similar to modulus, but does not work properly with negative numbers.*/
+
+const remainder = 11 % 3;
+
+
+//Augmented Math opearations
+
+//  += operator.
+/*
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+*/
+
+// -= subtracts a number from a variable.
+/*
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+*/
+
+//The *= operator multiplies a variable by a number.
+/*
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+*/
+
+//The /= operator divides a variable by another number.
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
