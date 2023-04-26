da811a36227bbc7f923846dc339695792b437a25
such-in <sachinmaurya1710@gmail.com>
2023-04-26 21:22:06 +0530

    Rest Parameters with functions

--- function.js
+++ function.js
@@ -71,3 +71,19 @@ const doubler = item => item * 2;
 const multiplier = (item, multi) => item * multi;
 multiplier(4, 2);
 ///multiplier(4, 2) would return the value 8.
+
+
+// Rest Parameter with Function Parameters
+/*ES6 introduces the rest parameter for function parameters. With the rest parameter, you can create functions that take a variable number
+of arguments. These arguments are stored in an array that can be accessed later from inside the function.
+
+Check out this code:
+*/
+function howMany(...args) {
+  return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2));
+console.log(howMany("string", null, [1, 2, 3], { }));
+//The console would display the strings You have passed 3 arguments. and You have passed 4 arguments..
+
+//The rest parameter eliminates the need to use the arguments object and allows us to use array methods on the array of parameters passed to the function howMany.
