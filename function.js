1b412a191b36cfec5fdc023291dbf35f198cd99f
such-in <sachinmaurya1710@gmail.com>
2023-04-26 21:16:21 +0530

    arrow function

--- function.js
+++ function.js
@@ -40,3 +40,20 @@ function randomNum(max){
 
 console.log(randomNum(50));
 
+//Arrow Functions to Write Concise Anonymous Functions
+/*we often don't need to name our functions, especially when passing a function as an argument to another function. Instead, we create inline functions. We don't need to name these functions because we do not reuse them anywhere else.
+
+To achieve this, we often use the following syntax:
+
+const myFunc = function() {
+  const myVar = "value";
+  return myVar;
+}
+ES6 provides us with the syntactic sugar to not have to write anonymous functions this way. Instead, you can use arrow function syntax:
+*/
+const myFunc = () => {
+  const myVar = "value";
+  return myVar;
+}
+
+  
